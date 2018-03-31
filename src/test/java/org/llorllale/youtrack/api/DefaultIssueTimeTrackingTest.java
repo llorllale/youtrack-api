999564a648936f95228853565b5264708278e20d
George Aristy <george.aristy@gmail.com>
2018-03-31 17:13:33 -0400

    (#168) Checkstyle issues on some test classes (#170)
    * These were fixed: DefaultIssueTimeTrackingTest,
      DefaultProjectTimeTrackingIT, XmlCommentTest, XmlUserTest,
      XmlUsersOfIssueIT

--- src/test/java/org/llorllale/youtrack/api/DefaultIssueTimeTrackingTest.java
+++ src/test/java/org/llorllale/youtrack/api/DefaultIssueTimeTrackingTest.java
@@ -16,8 +16,10 @@
 
 package org.llorllale.youtrack.api;
 
+// @checkstyle AvoidStaticImport (2 lines)
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 import org.llorllale.youtrack.api.mock.MockIssue;
 import org.llorllale.youtrack.api.mock.MockProject;
@@ -29,39 +31,46 @@ import org.llorllale.youtrack.api.mock.http.response.MockOkResponse;
  * Unit tests for {@link DefaultIssueTimeTracking}.
  * @author George Aristy (george.aristy@gmail.com)
  * @since 0.4.0
+ * @checkstyle MultipleStringLiterals (500 lines)
  */
-public class DefaultIssueTimeTrackingTest {
+public final class DefaultIssueTimeTrackingTest {
+  private static final String WORKITEMS =
+    "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+    + "<workItems>\n"
+    // @checkstyle LineLength (1 line)
+    + "  <workItem url=\"http://unit-258.labs.intellij.net:8080/charisma/rest/issue/HBR-63/timetracking/workitem/101-1\">\n"
+    + "    <id>101-1</id>\n"
+    + "    <date>1480204800000</date>\n"
+    + "    <duration>240</duration>\n"
+    + "    <description>first work item</description>\n"
+    // @checkstyle LineLength (1 line)
+    + "    <author login=\"root\" url=\"http://unit-258.labs.intellij.net:8080/charisma/rest/admin/user/root\"/>\n"
+    + "  </workItem>\n"
+    // @checkstyle LineLength (1 line)
+    + "  <workItem url=\"http://unit-258.labs.intellij.net:8080/charisma/rest/issue/HBR-63/timetracking/workitem/101-2\">\n"
+    + "    <id>101-2</id>\n"
+    + "    <date>1467936000000</date>\n"
+    + "    <duration>480</duration>\n"
+    + "    <description>second work item</description>\n"
+    // @checkstyle LineLength (1 line)
+    + "    <author login=\"root\" url=\"http://unit-258.labs.intellij.net:8080/charisma/rest/admin/user/root\"/>\n"
+    + "  </workItem>\n"
+    + "</workItems>";
+
+  /**
+   * Correctly counts the number of workItems returned by the YouTrack API.
+   * 
+   * @throws Exception unexpected
+   */
   @Test
-  public void testAll() throws Exception {
-    assertThat(new DefaultIssueTimeTracking(
-            new MockSession(), 
-            new MockIssue(
-                new MockProject("", "", "")
-            ), 
-            new MockHttpClient(
-                new MockOkResponse(WORKITEMS)
-            )
-        ).stream().count(),
-        is(2L)
+  public void countAll() throws Exception {
+    assertThat(
+      new DefaultIssueTimeTracking(
+        new MockSession(), 
+        new MockIssue(new MockProject()),
+        new MockHttpClient(new MockOkResponse(WORKITEMS))
+      ).stream().count(),
+      is(2L)
     );
   }
-
-  private static final String WORKITEMS =
-"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-"<workItems>\n" +
-"<workItem url=\"http://unit-258.labs.intellij.net:8080/charisma/rest/issue/HBR-63/timetracking/workitem/101-1\">\n" +
-"<id>101-1</id>\n" +
-"<date>1480204800000</date>\n" +
-"<duration>240</duration>\n" +
-"<description>first work item</description>\n" +
-"<author login=\"root\" url=\"http://unit-258.labs.intellij.net:8080/charisma/rest/admin/user/root\"/>\n" +
-"</workItem>\n" +
-"<workItem url=\"http://unit-258.labs.intellij.net:8080/charisma/rest/issue/HBR-63/timetracking/workitem/101-2\">\n" +
-"<id>101-2</id>\n" +
-"<date>1467936000000</date>\n" +
-"<duration>480</duration>\n" +
-"<description>second work item</description>\n" +
-"<author login=\"root\" url=\"http://unit-258.labs.intellij.net:8080/charisma/rest/admin/user/root\"/>\n" +
-"</workItem>\n" +
-"</workItems>";
 }
--- src/test/java/org/llorllale/youtrack/api/XmlUserTest.java
+++ src/test/java/org/llorllale/youtrack/api/XmlUserTest.java
@@ -16,19 +16,31 @@
 
 package org.llorllale.youtrack.api;
 
+// @checkstyle AvoidStaticImport (2 lines)
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  * Unit tests for {@link XmlUser}.
  * @author George Aristy (george.aristy@gmail.com)
  * @since 0.4.0
  */
-public class XmlUserTest {
+public final class XmlUserTest {
+  private static final String USER =
+    "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+    // @checkstyle LineLength (1 line)
+    + "<user lastCreatedProject=\"HBR\" login=\"exception\" email=\"vadim.gurov@gmail.com\" fullName=\"Application Exception\"/>";
+
   private static Xml xml;
 
+  /**
+   * Setup.
+   * 
+   * @throws Exception unexpected
+   */
   @BeforeClass
   public static void setUpClass() throws Exception {
     xml = new XmlOf(new StringAsDocument(USER));
@@ -36,14 +48,13 @@ public class XmlUserTest {
 
   /**
    * Test of name method, of class XmlUser.
-   * 
    * @since 0.4.0
    */
   @Test
   public void testName() {
     assertThat(
-        new XmlUser(xml).name(),
-        is("Application Exception")
+      new XmlUser(xml).name(),
+      is("Application Exception")
     );
   }
 
@@ -54,20 +65,19 @@ public class XmlUserTest {
   @Test
   public void testEmail() {
     assertThat(
-        new XmlUser(xml).email(),
-        is("vadim.gurov@gmail.com")
+      new XmlUser(xml).email(),
+      is("vadim.gurov@gmail.com")
     );
   }
 
+  /**
+   * Test of loginName method, of class XmlUser.
+   */
   @Test
   public void testLoginName() {
     assertThat(
-        new XmlUser(xml).loginName(),
-        is("exception")
+      new XmlUser(xml).loginName(),
+      is("exception")
     );
   }
-
-  private static final String USER =
-"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-"<user lastCreatedProject=\"HBR\" login=\"exception\" email=\"vadim.gurov@gmail.com\" fullName=\"Application Exception\"/>";
 }
