d9ce88c1c58a97037cef773596da6bc127b01bb6
George Aristy <george.aristy@gmail.com>
2018-03-31 19:44:55 -0400

    (#173) Fixed more checkstyle issues in test packages

--- src/test/java/org/llorllale/youtrack/api/XmlOfTest.java
+++ src/test/java/org/llorllale/youtrack/api/XmlOfTest.java
@@ -16,138 +16,141 @@
 
 package org.llorllale.youtrack.api;
 
-import java.util.Optional;
+// @checkstyle AvoidStaticImport (4 lines)
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.empty;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+
+import java.util.Optional;
 import org.junit.Test;
 
 /**
  * Unit tests for {@link Xml}.
- *
  * @author George Aristy (george.aristy@gmail.com)
  * @since 1.0.0
+ * @checkstyle MethodName (500 lines)
+ * @checkstyle MultipleStringLiterals (500 lines)
  */
-public class XmlOfTest {
+public final class XmlOfTest {
   /**
    * {@link Xml#textOf(java.lang.String)} must return the string value if given an 
    * valid xpath.
-   * 
+   * @throws Exception unexpected
    * @since 1.0.0
    */
   @Test
   public void textAtWithValidXpath() throws Exception {
     assertThat(
-        new XmlOf(new StringAsDocument(
-            "<issue id='123'>" +
-            "  <summary>Test Summary</summary>" +
-            "  <description>Test Description</description>" +
-            "</issue>"
-        )).textOf("@id"),
-        is(Optional.of("123"))
+      new XmlOf(new StringAsDocument(
+        "<issue id='123'>"
+        + "  <summary>Test Summary</summary>"
+        + "  <description>Test Description</description>"
+        + "</issue>"
+      )).textOf("@id"),
+      is(Optional.of("123"))
     );
   }
 
   /**
    * Result of {@link Xml#textOf(java.lang.String)} with an invalid xpath must be an empty
    * optional.
-   * 
+   * @throws Exception unexpected
    * @since 1.0.0
    */
   @Test
   public void textAtWithInvalidXpath() throws Exception {
     assertThat(
-        new XmlOf(new StringAsDocument(
-            "<issue id='123'>" +
-            "  <summary>Test Summary</summary>" +
-            "  <description>Test Description</description>" +
-            "</issue>"
-        )).textOf("//asdf"),
-        is(Optional.empty())
+      new XmlOf(new StringAsDocument(
+        "<issue id='123'>"
+        + "  <summary>Test Summary</summary>"
+        + "  <description>Test Description</description>"
+        + "</issue>"
+      )).textOf("//asdf"),
+      is(Optional.empty())
     );
   }
 
   /**
    * Result of {@link Xml#child(java.lang.String)} with an invalid xpath must be an empty
    * optional.
-   * 
+   * @throws Exception unexpected
    * @since 1.0.0
    */
   @Test
   public void childWithInvalidXpath() throws Exception {
     assertFalse(
-        new XmlOf(new StringAsDocument(
-            "<issue id='123'>" +
-            "  <summary>Test Summary</summary>" +
-            "  <description>Test Description</description>" +
-            "</issue>"
-        )).child("//asdf").isPresent()
+      new XmlOf(new StringAsDocument(
+        "<issue id='123'>"
+        + "  <summary>Test Summary</summary>"
+        + "  <description>Test Description</description>"
+        + "</issue>"
+      )).child("//asdf").isPresent()
     );
   }
 
   /**
    * {@link Xml#child(java.lang.String)} with a valid xpath must return the correct 
-   * {@link Xml}
-   * 
+   * {@link Xml}.
+   * @throws Exception unexpected
    * @since 1.0.0
    */
   @Test
   public void childWithValidXpath() throws Exception {
     assertThat(
-        new XmlOf(new StringAsDocument(
-            "<issue id='123'>" +
-            "  <summary>Test Summary</summary>" +
-            "  <description>Test Description</description>" +
-            "</issue>"
-        )).child("description").get().textOf("text()").get(),
-        is("Test Description")
+      new XmlOf(new StringAsDocument(
+        "<issue id='123'>"
+        + "  <summary>Test Summary</summary>"
+        + "  <description>Test Description</description>"
+        + "</issue>"
+      )).child("description").get().textOf("text()").get(),
+      is("Test Description")
     );
   }
 
   /**
    * Result of {@link Xml#children(java.lang.String)} with valid xpath should consist of a 
    * collection of the expected size.
-   * 
+   * @throws Exception unexpected
    * @since 1.0.0
    */
   @Test
   public void testChildrenWithValidXpath() throws Exception {
     assertThat(
-        new XmlOf(new StringAsDocument(
-            "<issue id='123'>" +
-            "  <summary>Test Summary</summary>" +
-            "  <description>Test Description</description>" +
-            "  <fields>" +
-            "    <field name='field1'/>" +
-            "    <field name='field2'/>" +
-            "  </fields>" +
-            "</issue>"
-        )).children("//field").size(),
-        is(2)
+      new XmlOf(new StringAsDocument(
+        "<issue id='123'>"
+        + "  <summary>Test Summary</summary>"
+        + "  <description>Test Description</description>"
+        + "  <fields>"
+        + "    <field name='field1'/>"
+        + "    <field name='field2'/>"
+        + "  </fields>"
+        + "</issue>"
+      )).children("//field").size(),
+      is(2)
     );
   }
 
   /**
    * Result of {@link Xml#children(java.lang.String)} with an invalid xpath should be an 
    * empty collection.
-   * 
+   * @throws Exception unexpected
    * @since 1.0.0
    */
   @Test
   public void testChildrenWithInvalidXpath() throws Exception {
     assertThat(
-        new XmlOf(new StringAsDocument(
-            "<issue id='123'>" +
-            "  <summary>Test Summary</summary>" +
-            "  <description>Test Description</description>" +
-            "  <fields>" +
-            "    <field name='field1'/>" +
-            "    <field name='field2'/>" +
-            "  </fields>" +
-            "</issue>"
-        )).children("//asdf"),
-        is(empty())
+      new XmlOf(new StringAsDocument(
+        "<issue id='123'>"
+        + "  <summary>Test Summary</summary>"
+        + "  <description>Test Description</description>"
+        + "  <fields>"
+        + "    <field name='field1'/>"
+        + "    <field name='field2'/>"
+        + "  </fields>"
+        + "</issue>"
+      )).children("//asdf"),
+      is(empty())
     );
   }
 }
--- src/test/java/org/llorllale/youtrack/api/XmlProjectFieldTest.java
+++ src/test/java/org/llorllale/youtrack/api/XmlProjectFieldTest.java
@@ -16,10 +16,12 @@
 
 package org.llorllale.youtrack.api;
 
+// @checkstyle AvoidStaticImport (4 lines)
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.llorllale.youtrack.api.mock.MockField;
@@ -28,100 +30,130 @@ import org.llorllale.youtrack.api.mock.http.MockSession;
 
 /**
  * Unit tests for {@link XmlProjectField}.
- * 
  * @author George Aristy (george.aristy@gmail.com)
  * @since 0.8.0
+ * @checkstyle MultipleStringLiterals (500 lines)
+ * @checkstyle MethodName (500 lines)
  */
-public class XmlProjectFieldTest {
+public final class XmlProjectFieldTest {
+  private static final String XML =
+    // @checkstyle LineLength (1 line)
+    "<projectCustomField name=\"Priority\" url=\"http://localhost/rest/admin/project/TP/customfield/Priority\"/>"; 
+
   private static Xml xml;
 
+  /**
+   * Setup.
+   * @throws Exception unexpected
+   */
   @BeforeClass
   public static void setup() throws Exception {
     xml = new XmlOf(new StringAsDocument(XML));
   }
 
+  /**
+   * Returns the xml's @name.
+   */
   @Test
   public void testName() {
     assertThat(
-        new XmlProjectField(xml, new MockProject(), new MockSession()).name(),
-        is("Priority")
+      new XmlProjectField(xml, new MockProject(), new MockSession()).name(),
+      is("Priority")
     );
   }
 
+  /**
+   * Must equal itself.
+   */
   @Test
   public void equalsItself() {
-    final Field f = new XmlProjectField(xml, new MockProject(), new MockSession());
-
+    final Field field = new XmlProjectField(xml, new MockProject(), new MockSession());
     assertTrue(
-        f.equals(f)
+      field.equals(field)
     );
   }
 
+  /**
+   * Must equal other field with same name.
+   */
   @Test
   public void equalsOtherField() {
     assertTrue(
-        new XmlProjectField(
-            xml, 
-            new MockProject(), 
-            new MockSession()
-        ).equals(
-            new MockField("Priority", new MockProject())
-        )
+      new XmlProjectField(
+        xml, 
+        new MockProject(), 
+        new MockSession()
+      ).equals(
+        new MockField("Priority", new MockProject())
+      )
     );
   }
 
+  /**
+   * Cannot equal {@code null}.
+   */
   @Test
   public void notEqualsNull() {
     assertFalse(
-        new XmlProjectField(
-            xml, 
-            new MockProject(), 
-            new MockSession()
-        ).equals(null)
+      new XmlProjectField(
+        xml, 
+        new MockProject(), 
+        new MockSession()
+      ).equals(null)
     );
   }
 
+  /**
+   * Cannot equal object that is not a ProjectField.
+   */
   @Test
   public void notEqualsObject() {
     assertFalse(
-        new XmlProjectField(
-            xml, 
-            new MockProject(), 
-            new MockSession()
-        ).equals(new Object())
+      new XmlProjectField(
+        xml, 
+        new MockProject(), 
+        new MockSession()
+      ).equals(new Object())
     );
   }
 
+  /**
+   * Cannot equal a field witha different name.
+   * @throws Exception unexpected
+   */
   @Test
   public void notEqualsFieldWithDiffName() throws Exception {
     assertFalse(
-        new XmlProjectField(
-            new XmlOf(new StringAsDocument(
-                "<projectCustomField name=\"name1\" url=\"http://localhost/rest/admin/project/TP/customfield/Priority\"/>"
-            )), 
-            new MockProject(), 
-            new MockSession()
-        ).equals(
-            new MockField("name2", new MockProject())
-        )
+      new XmlProjectField(
+        new XmlOf(new StringAsDocument(
+          // @checkstyle LineLength (1 line)
+          "<projectCustomField name=\"name1\" url=\"http://localhost/rest/admin/project/TP/customfield/Priority\"/>"
+        )), 
+        new MockProject(), 
+        new MockSession()
+      ).equals(
+        new MockField("name2", new MockProject())
+      )
     );
   }
 
+  /**
+   * Cannot equal a field from another project.
+   * @throws Exception unexpected
+   */
   @Test
   public void notEqualsFieldFromDiffProject() throws Exception {
     assertFalse(
-        new XmlProjectField(
-            new XmlOf(new StringAsDocument(
-                "<projectCustomField name=\"name\" url=\"http://localhost/rest/admin/project/TP/customfield/Priority\"/>"
-            )), 
-            new MockProject("p1", "p1", ""), 
-            new MockSession()
-        ).equals(
-            new MockField("name", new MockProject("p2", "p2", ""))
-        )
+      new XmlProjectField(
+        new XmlOf(new StringAsDocument(
+          // @checkstyle LineLength (1 line)
+          "<projectCustomField name=\"name\" url=\"http://localhost/rest/admin/project/TP/customfield/Priority\"/>"
+        )), 
+        new MockProject("p1", "p1", ""), 
+        new MockSession()
+      ).equals(
+        new MockField("name", new MockProject("p2", "p2", ""))
+      )
     );
   }
-
-  private static final String XML =
-"<projectCustomField name=\"Priority\" url=\"http://localhost/rest/admin/project/TP/customfield/Priority\"/>"; 
 }
