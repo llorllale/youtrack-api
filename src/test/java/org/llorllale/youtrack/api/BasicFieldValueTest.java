56821620537ccf2c3cbecdf8a12e909eee163176
George Aristy <george.aristy@gmail.com>
2018-03-31 21:07:10 -0400

    (#175) Fixed more checkstyle issues in test packages (#176)

--- src/test/java/org/llorllale/youtrack/api/BasicFieldValueTest.java
+++ src/test/java/org/llorllale/youtrack/api/BasicFieldValueTest.java
@@ -16,79 +16,105 @@
 
 package org.llorllale.youtrack.api;
 
+// @checkstyle AvoidStaticImport (4 lines)
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.llorllale.youtrack.api.mock.MockField;
 import org.llorllale.youtrack.api.mock.MockFieldValue;
 import org.llorllale.youtrack.api.mock.MockProject;
 
 /**
  * Unit tests for {@link BasicFieldValue}.
- *
  * @author George Aristy (george.aristy@gmail.com)
- * @since 0.8.0-SNAPSHOT
+ * @since 0.8.0
+ * @checkstyle MultipleStringLiterals (500 lines)
+ * @checkstyle MethodName (500 lines)
  */
-public class BasicFieldValueTest {
+public final class BasicFieldValueTest {
+  /**
+   * Returns given string.
+   */
   @Test
   public void testAsString() {
     final String string = "test";
-
     assertThat(
-        new BasicFieldValue(string, null).asString(),
-        is(string)
+      new BasicFieldValue(string, null).asString(),
+      is(string)
     );
   }
 
+  /**
+   * Cannot equal another type.
+   */
   @Test
   public void notEqualsWithObject() {
     assertFalse(
-        new BasicFieldValue("", new MockField("test", new MockProject())).equals(new Object())
+      new BasicFieldValue("", new MockField("test", new MockProject())).equals(new Object())
     );
   }
 
+  /**
+   * Cannot equal null.
+   */
   @Test
   public void notEqualsWithNull() {
     assertFalse(
-        new BasicFieldValue("", new MockField("test", new MockProject())).equals(null)
+      new BasicFieldValue("", new MockField("test", new MockProject())).equals(null)
     );
   }
 
+  /**
+   * Cannot field another fieldValue with different value.
+   */
   @Test
   public void notEqualsWithDiffFieldValue() {
     final Field field = new MockField("test", new MockProject());
     assertFalse(
-        new BasicFieldValue(
-            "value1", 
-            field
-        ).equals(new MockFieldValue(field, "value2"))
+      new BasicFieldValue(
+        "value1",
+        field
+      ).equals(new MockFieldValue(field, "value2"))
     );
   }
 
+  /**
+   * Cannot equal another fieldValue with different field name.
+   */
   @Test
   public void notEqualsWithDiffFields() {
     assertFalse(
-        new BasicFieldValue(
-            "value", 
-            new MockField("field1", new MockProject())
-        ).equals(
-            new MockFieldValue(
-                new MockField("field2", new MockProject()), 
-                "value"
-            )
+      new BasicFieldValue(
+        "value",
+        new MockField("field1", new MockProject())
+      ).equals(
+        new MockFieldValue(
+          new MockField("field2", new MockProject()),
+          "value"
         )
+      )
     );
   }
 
+  /**
+   * Must equal itself.
+   */
   @Test
   public void equalsItself() {
-    final BasicFieldValue fv = new BasicFieldValue("value", new MockField("field", new MockProject()));
-
+    final BasicFieldValue fieldValue = new BasicFieldValue(
+      "value", new MockField("field", new MockProject())
+    );
     assertTrue(
-        fv.equals(fv)
+      fieldValue.equals(fieldValue)
     );
   }
 
+  /**
+   * Must equal another fieldValue provided that both the name and value are equal.
+   */
   @Test
   public void equalsOtherFieldValue() {
     final Field field = new MockField("field", new MockProject());
--- src/test/java/org/llorllale/youtrack/api/XmlUsersOfProjectIT.java
+++ src/test/java/org/llorllale/youtrack/api/XmlUsersOfProjectIT.java
@@ -16,25 +16,31 @@
 
 package org.llorllale.youtrack.api;
 
+// @checkstyle AvoidStaticImport (3 lines)
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import org.junit.Test;
+
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.llorllale.youtrack.api.session.PermanentToken;
 import org.llorllale.youtrack.api.session.Session;
 
 /**
  * Integration tests for {@link XmlUsersOfProject}.
- *
  * @author George Aristy (george.aristy@gmail.com)
  * @since 0.8.0
+ * @checkstyle AbbreviationAsWordInName (2 lines)
  */
-public class XmlUsersOfProjectIT {
+public final class XmlUsersOfProjectIT {
   private static IntegrationTestsConfig config;
   private static Session session;
   private static Project project;
 
+  /**
+   * Setup.
+   * @throws Exception unexpected
+   */
   @BeforeClass
   public static void setup() throws Exception {
     config = new IntegrationTestsConfig();
@@ -42,35 +48,52 @@ public class XmlUsersOfProjectIT {
     project = new DefaultYouTrack(session).projects().stream().findAny().get();
   }
 
+  /**
+   * Returns the pre-configured user.
+   * @throws Exception unexpected
+   */
   @Test
   public void testUser() throws Exception {
-    assertThat(new XmlUsersOfProject(project, session, this.xmlObject("random"))
-            .user(config.youtrackUser())
-            .loginName(),
-        is(config.youtrackUser())
+    assertThat(
+      new XmlUsersOfProject(project, session, this.xmlObject("random"))
+        .user(config.youtrackUser())
+        .loginName(),
+      is(config.youtrackUser())
     );
   }
 
+  /**
+   * Returns the pre-configured assignee.
+   * @throws Exception unexpected
+   */
   @Test
   public void testAssignees() throws Exception {
-    assertTrue(new XmlUsersOfProject(project, session, this.xmlObject(config.youtrackUser()))
-            .assignees()
-            .anyMatch(a -> config.youtrackUser().equals(a.loginName()))
+    assertTrue(
+      new XmlUsersOfProject(project, session, this.xmlObject(config.youtrackUser()))
+        .assignees()
+        .anyMatch(a -> config.youtrackUser().equals(a.loginName()))
     );
   }
 
+  /**
+   * Mock XML.
+   * @param assigneeLogin mock assignee login name
+   * @return mock XML
+   * @throws Exception unexpected
+   */
   private Xml xmlObject(String assigneeLogin) throws Exception {
-    final String TEMPLATE = 
-        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-        "<project>\n" +
-        "  <assigneesLogin>\n" +
-        "    <sub value=\"%s\"/>\n" +
-        "  </assigneesLogin>\n" +
-        "</project>";
     return new XmlOf(
-        new StringAsDocument(
-            String.format(TEMPLATE, assigneeLogin)
+      new StringAsDocument(
+        String.format(
+          "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+          + "<project>\n"
+          + "  <assigneesLogin>\n"
+          + "    <sub value=\"%s\"/>\n"
+          + "  </assigneesLogin>\n"
+          + "</project>",
+          assigneeLogin
         )
+      )
     );
   }
 }
