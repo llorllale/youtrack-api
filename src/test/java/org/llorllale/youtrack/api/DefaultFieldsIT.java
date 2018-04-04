00059e1f23c2a5839ff49b0d297fbc7b9f5f1d59
George Aristy <george.aristy@gmail.com>
2018-04-04 13:59:16 -0400

    (#183) Fixed more checkstyle issues

--- src/test/java/org/llorllale/youtrack/api/DefaultFieldsIT.java
+++ src/test/java/org/llorllale/youtrack/api/DefaultFieldsIT.java
@@ -16,9 +16,11 @@
 
 package org.llorllale.youtrack.api;
 
+// @checkstyle AvoidStaticImport (3 lines)
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.llorllale.youtrack.api.session.PermanentToken;
@@ -26,26 +28,36 @@ import org.llorllale.youtrack.api.session.Session;
 
 /**
  * Integration tests for {@link DefaultFields}.
- * 
  * @author George Aristy (george.aristy@gmail.com)
  * @since 0.8.0
+ * @checkstyle AbbreviationAsWordInName (2 lines)
  */
-public class DefaultFieldsIT {
+public final class DefaultFieldsIT {
   private static Session session;
   private static Project project;
 
+  /**
+   * Setup.
+   * @throws Exception unexpected
+   */
   @BeforeClass
   public static void setup() throws Exception {
     final IntegrationTestsConfig config = new IntegrationTestsConfig();
-    session = new PermanentToken(config.youtrackUrl(), config.youtrackUserToken()).login();
+    session = new PermanentToken(
+      config.youtrackUrl(), config.youtrackUserToken()
+    ).login();
     project = new DefaultYouTrack(session).projects().stream().findAny().get();
   }
 
+  /**
+   * Stream returns the fields pre-configured for the project.
+   * @throws Exception unexpected
+   */
   @Test
   public void testStream() throws Exception {
     assertThat(
-        new DefaultFields(session, project).stream().count(),
-        is(greaterThan(0L))
+      new DefaultFields(session, project).stream().count(),
+      is(greaterThan(0L))
     );
   }
 }
--- src/test/java/org/llorllale/youtrack/api/DefaultFieldsTest.java
+++ src/test/java/org/llorllale/youtrack/api/DefaultFieldsTest.java
@@ -16,10 +16,12 @@
 
 package org.llorllale.youtrack.api;
 
-import java.util.stream.Collectors;
+// @checkstyle AvoidStaticImport (3 lines)
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertThat;
+
+import java.util.stream.Collectors;
 import org.junit.Test;
 import org.llorllale.youtrack.api.mock.MockProject;
 import org.llorllale.youtrack.api.mock.http.MockHttpClient;
@@ -28,47 +30,46 @@ import org.llorllale.youtrack.api.mock.http.response.MockOkResponse;
 
 /**
  * Unit tests for {@link DefaultFields}.
- *
  * @author George Aristy (george.aristy@gmail.com)
  * @since 1.0.0
  */
-public class DefaultFieldsTest {
+public final class DefaultFieldsTest {
   /**
    * {@link DefaultFields#project()} must return the same project.
-   * 
    * @since 1.0.0
    */
   @Test
   public void project() {
     final Project project = new MockProject();
     assertThat(
-        new DefaultFields(null, project).project(),
-        is(project)
+      new DefaultFields(null, project).project(),
+      is(project)
     );
   }
 
   /**
-   * {@link DefaultFields#stream()} must return all fields returned by the server.
-   * 
-   * @throws Exception 
+   * {@link DefaultFields#stream()} must return all fields returned by the
+   * server.
+   * @throws Exception unexpected
    * @since 1.0.0
    */
   @Test
   public void stream() throws Exception {
     assertThat(
-        new DefaultFields(
-            new MockSession(), 
-            new MockProject(), 
-            new MockHttpClient(
-                new MockOkResponse(
-                    "<projectCustomFieldRefs>\n" +
-                    "  <projectCustomField name=\"Pyatiletka\" url=\"http://youtrack.jetbrains.net/rest/admin/project/CMN/customfield/Plan\"/>\n" +
-                    "  <projectCustomField name=\"Region\" url=\"http://youtrack.jetbrains.net/rest/admin/project/CMN/customfield/Region\"/>\n" +
-                    "</projectCustomFieldRefs>"
-                )
-            )
-        ).stream().map(ProjectField::name).collect(Collectors.toList()),
-        containsInAnyOrder("Pyatiletka", "Region")
+      new DefaultFields(
+        new MockSession(),
+        new MockProject(),
+        new MockHttpClient(
+          new MockOkResponse(
+            "<projectCustomFieldRefs>\n"
+            // @checkstyle LineLength (2 lines)
+            + "  <projectCustomField name=\"Pyatiletka\" url=\"http://youtrack.jetbrains.net/rest/admin/project/CMN/customfield/Plan\"/>\n"
+            + "  <projectCustomField name=\"Region\" url=\"http://youtrack.jetbrains.net/rest/admin/project/CMN/customfield/Region\"/>\n"
+            + "</projectCustomFieldRefs>"
+          )
+        )
+      ).stream().map(ProjectField::name).collect(Collectors.toList()),
+      containsInAnyOrder("Pyatiletka", "Region")
     );
   }
 }
