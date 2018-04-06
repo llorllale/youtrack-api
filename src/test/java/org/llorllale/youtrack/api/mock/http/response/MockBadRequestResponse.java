11f4302bb3d1c8c14150da4e72fb1a20408b31ab
George Aristy <george.aristy@gmail.com>
2018-04-06 17:10:46 -0400

    (#188) Fixed more checkstyle issues in test packages

--- src/test/java/org/llorllale/youtrack/api/mock/http/response/MockBadRequestResponse.java
+++ src/test/java/org/llorllale/youtrack/api/mock/http/response/MockBadRequestResponse.java
@@ -30,23 +30,23 @@ import org.apache.http.params.HttpParams;
 /**
  * Mock impl. of {@link HttpResponse} that simulates an HTTP response from YouTrack with code
  * {@code 400}.
- *
  * @author George Aristy (george.aristy@gmail.com)
  * @since 1.0.0
  */
+@SuppressWarnings("checkstyle:MethodCount")
 public final class MockBadRequestResponse implements HttpResponse {
   private final StatusLine statusLine;
 
   /**
    * Ctor.
-   * 
    * @since 1.0.0
    */
   public MockBadRequestResponse() {
     this.statusLine = new BasicStatusLine(
-        new HttpVersion(1, 1), 
-        400, 
-        "Bad Request"
+      new HttpVersion(1, 1),
+      // @checkstyle MagicNumber (1 line)
+      400,
+      "Bad Request"
     );
   }
 
@@ -57,132 +57,132 @@ public final class MockBadRequestResponse implements HttpResponse {
 
   @Override
   public void setStatusLine(StatusLine statusline) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setStatusLine(ProtocolVersion ver, int code) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setStatusLine(ProtocolVersion ver, int code, String reason) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setStatusCode(int code) throws IllegalStateException {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setReasonPhrase(String reason) throws IllegalStateException {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public HttpEntity getEntity() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setEntity(HttpEntity entity) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public Locale getLocale() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setLocale(Locale loc) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public ProtocolVersion getProtocolVersion() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public boolean containsHeader(String name) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public Header[] getHeaders(String name) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public Header getFirstHeader(String name) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public Header getLastHeader(String name) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public Header[] getAllHeaders() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void addHeader(Header header) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void addHeader(String name, String value) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setHeader(Header header) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setHeader(String name, String value) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setHeaders(Header[] headers) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void removeHeader(Header header) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void removeHeaders(String name) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public HeaderIterator headerIterator() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public HeaderIterator headerIterator(String name) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public HttpParams getParams() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setParams(HttpParams params) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
 }
--- src/test/java/org/llorllale/youtrack/api/mock/http/response/MockOkResponse.java
+++ src/test/java/org/llorllale/youtrack/api/mock/http/response/MockOkResponse.java
@@ -36,7 +36,8 @@ import org.apache.http.params.HttpParams;
  * @author George Aristy (george.aristy@gmail.com)
  * @since 0.4.0
  */
-public class MockOkResponse implements HttpResponse {
+@SuppressWarnings("checkstyle:MethodCount")
+public final class MockOkResponse implements HttpResponse {
   private final StatusLine statusLine;
   private final HttpEntity payload;
   private final List<Header> headers;
@@ -50,9 +51,10 @@ public class MockOkResponse implements HttpResponse {
    */
   public MockOkResponse(HttpEntity payload, Header... headers) {
     this.statusLine = new BasicStatusLine(
-        new ProtocolVersion("HTTP", 1, 1), 
-        200, 
-        "OK"
+      new ProtocolVersion("HTTP", 1, 1), 
+      // @checkstyle MagicNumber (1 line)
+      200, 
+      "OK"
     );
     this.payload = payload;
     this.headers = Arrays.asList(headers);
@@ -60,7 +62,7 @@ public class MockOkResponse implements HttpResponse {
 
   /**
    * Sets the {@link #getEntity() payload} to {@code null}.
-   * @param headers 
+   * @param headers headers
    * @since 0.4.0
    */
   public MockOkResponse(Header... headers) {
@@ -69,8 +71,8 @@ public class MockOkResponse implements HttpResponse {
 
   /**
    * Sets the {@link #getEntity() payload} to {@code null}.
+   * @see #MockOkResponse(HttpEntity) 
    * @since 0.4.0
-   * @see #MockOkResponse(org.apache.http.HttpEntity) 
    */
   public MockOkResponse() {
     this((HttpEntity) null);
@@ -80,8 +82,8 @@ public class MockOkResponse implements HttpResponse {
    * Uses {@code payload} as an {@link HttpEntity} with content type 
    * {@link ContentType#APPLICATION_XML}.
    * @param payload the mock payload
+   * @see #MockOkResponse(HttpEntity) 
    * @since 0.4.0
-   * @see #MockOkResponse(org.apache.http.HttpEntity) 
    */
   public MockOkResponse(String payload) {
     this(new StringEntity(payload, ContentType.APPLICATION_XML));
@@ -89,136 +91,136 @@ public class MockOkResponse implements HttpResponse {
 
   @Override
   public StatusLine getStatusLine() {
-    return statusLine;
+    return this.statusLine;
   }
 
   @Override
   public void setStatusLine(StatusLine statusline) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setStatusLine(ProtocolVersion ver, int code) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setStatusLine(ProtocolVersion ver, int code, String reason) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setStatusCode(int code) throws IllegalStateException {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setReasonPhrase(String reason) throws IllegalStateException {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public HttpEntity getEntity() {
-    return payload;
+    return this.payload;
   }
 
   @Override
   public void setEntity(HttpEntity entity) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public Locale getLocale() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setLocale(Locale loc) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public ProtocolVersion getProtocolVersion() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public boolean containsHeader(String name) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public Header[] getHeaders(String name) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public Header getFirstHeader(String name) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public Header getLastHeader(String name) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public Header[] getAllHeaders() {
-    return headers.toArray(new Header[]{});
+    return this.headers.toArray(new Header[] {});
   }
 
   @Override
   public void addHeader(Header header) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void addHeader(String name, String value) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setHeader(Header header) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setHeader(String name, String value) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
-  public void setHeaders(Header[] headers) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+  public void setHeaders(Header[] hdrs) {
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void removeHeader(Header header) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void removeHeaders(String name) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public HeaderIterator headerIterator() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public HeaderIterator headerIterator(String name) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public HttpParams getParams() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 
   @Override
   public void setParams(HttpParams params) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO implement
+    throw new UnsupportedOperationException("Not supported yet.");
   }
 }
